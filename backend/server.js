849a340e94271ffcaf4578d78ffd0c42d7af61d9
nicofzzn <nicofaza975@gmail.com>
2020-10-17 07:40:33 +0700

    custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -9,14 +11,12 @@ connectDB()
 
 const app = express()
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+// error middleware
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
