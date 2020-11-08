750c89a5b020fab1210bd92c66129ba9f273c128
nicofzzn <nicofaza975@gmail.com>
2020-11-08 22:47:37 +0700

    admin delete product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -7,6 +7,9 @@ import {
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_CLEANUP,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
 } from '../constants/productConstants'
 
 export const listProducts = () => async dispatch => {
@@ -49,4 +52,36 @@ export const cleanProductDetails = () => async dispatch => {
   try {
     dispatch({ type: PRODUCT_DETAILS_CLEANUP })
   } catch (error) {}
+}
+
+export const deleteProduct = id => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_DELETE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    await axios.delete(`/api/products/${id}`, config)
+
+    dispatch({
+      type: PRODUCT_DELETE_SUCCESS,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,92 @@
+import React, { useEffect } from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProducts, deleteProduct } from '../actions/productActions'
+
+const ProductListScreen = ({ history, match }) => {
+  const dispatch = useDispatch()
+  const { loading, error, products } = useSelector(state => state.productList)
+  const { userInfo } = useSelector(state => state.userLogin)
+  const {
+    success: successDelete,
+    error: errorDelete,
+    loading: loadingDelete,
+  } = useSelector(state => state.productDelete)
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProducts())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, userInfo, successDelete, history])
+
+  const deleteHandler = id => {
+    dispatch(deleteProduct(id))
+  }
+  const createProductHandler = id => {}
+
+  return (
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-right'>
+          <Button className='my-3' onClick={createProductHandler}>
+            <i className='fa fa-plus'></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {errorDelete && <Loader variant='danger'>{errorDelete}</Loader>}
+      {loading || loadingDelete ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className='table-sm'>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>NAME</td>
+              <td>PRICE</td>
+              <td>CATEGORY</td>
+              <td>BRAND</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Button variant='light' className='btn-sm'>
+                      <i className='fas fa-edit'></i>
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    variant='danger'
+                    className='btn-sm'
+                    onClick={() => deleteHandler(product._id)}
+                  >
+                    <i className='fas fa-trash'></i>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  )
+}
+
+export default ProductListScreen
