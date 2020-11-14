062ff6542fb145e54f8202de85be903d8d2011a4
nicofzzn <nicofaza975@gmail.com>
2020-11-14 12:49:26 +0700

    product pagnation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,8 @@ import asyncHandler from 'express-async-handler'
 // @route GET /api/products
 // @access public
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 5
+  const page = Number(req.query.pageNumber) || 1
   const keyword = req.query.keyword
     ? {
         name: {
@@ -14,9 +16,12 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {}
 
+  const count = await Product.countDocuments({ ...keyword })
   const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
 
-  res.json(products)
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 // @desc fetch single products
